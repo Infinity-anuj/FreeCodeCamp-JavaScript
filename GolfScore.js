39cfc25c0b648a9a5b207d4175ba0a6213426c22
Infinity-anuj <anuj.patel.9903@gmail.com>
2026-08-28 15:38:16 +0530

    New Chapter Practice

---
+++ GolfScore.js
@@ -0,0 +1,34 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par,stroke)
+{
+    if(stroke === 1){
+        return "Hole-in-one!";
+    }
+    else if(stroke <= par - 2)
+    {
+        return "Eagle";
+    }
+    else if(stroke === par - 1)
+    {
+        return "Birdie";
+    }
+    else if(stroke === par)
+    {
+        return "Par";
+    }
+    else if(stroke === par + 1 )
+    {
+        return "Bogey";
+    }
+    else if(stroke === par + 2)
+    {
+        return "Double Bogey";
+    }
+    else if(stroke > par + 3){
+        return "Go home!";
+    }
+}
+
+console.log(golfScore(3, 7));
+console.log(golfScore(4, 8));
